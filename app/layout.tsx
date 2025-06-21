e424f9dc7b832ad29e6039d5aa29ef7cafa1efd1
Yash Kumar <121498975+imrja8@users.noreply.github.com>
2025-06-21 11:30:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,21 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-import { baseUrl, createMetadata } from "@/utils/metadata";
-
+import { baseUrl, createMetadata } from '@/utils/metadata';
 
 const inter = Inter({
   subsets: ['latin'],
 });
-
 
 export const metadata = createMetadata({
   title: {
     template: '%s | Tegota Docs',
     default: 'Tegota Docs',
   },
-  description: 'Tegota Docs is a prebuilt template for building documentation sites using Next.js and Fumadocs.',
+  description:
+    'Tegota Docs is a prebuilt template for building documentation sites using Next.js and Fumadocs.',
   metadataBase: baseUrl,
 });
-
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
